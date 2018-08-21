429c2be7003ecd8db6775cacdcf311b1baeac661
jeysunit <jeysunit@bcamp.net>
2018-08-21 23:08:17 +0900

    add docx template

--- make_docx.bat
+++ make_docx.bat
@@ -1,12 +1,18 @@
 @echo off
+cd /d %~dp0
 rem chcp 65001
 
 IF NOT DEFINED WORKSPACE set WORKSPACE=.
 
-set "pandocexe=C:\Program Files (x86)\Pandoc\pandoc.exe"
+set "pandocexe=C:\ProgramData\chocolatey\bin\pandoc.exe"
 set "PANDOC_MARKDOWN_OPT=-f markdown_github+tex_math_dollars+definition_lists+fancy_lists+pandoc_title_block+superscript"
 set "reffile=.\小見出し改ページテンプレート.docx"
+set IN_MARKDOWN=%1
 set "outfile=.\%~n1.docx"
 
-"%pandocexe%" --standalone %PANDOC_MARKDOWN_OPT% -smart --reference-doc="%reffile%" -t docx -o "%outfile%" %1
+echo markdown形式からdocx形式に変換します
+echo %IN_MARKDOWN%
+echo → %outfile%
+"%pandocexe%" --standalone %PANDOC_MARKDOWN_OPT% --reference-doc="%reffile%" --to=docx --output="%outfile%" %IN_MARKDOWN%
 
+pause >null
